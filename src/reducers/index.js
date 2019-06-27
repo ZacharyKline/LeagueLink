fc150eaf6514a1d262affa639e2bc67a5a8d2067
Nate <nate998877@gmail.com>
2019-06-27 14:23:23 -0400

    changed reducers index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,15 @@
-import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
-
-//import coaches from "./coaches";
-//import managers from "./managers";
-//import parents from "./parents";
-//import teams from "./teams";
+import { combineReducers } from "redux";
+import managers from "./managers";
+import coaches from "./coaches";
+import parents from "./parents";
+import teams from "./teams";
 
 export default history =>
   combineReducers({
-    router: connectRouter(history)
-    //coaches,
-    //managers,
-    //parents,
-    //teams
+    router: connectRouter(history),
+    coaches,
+    managers,
+    parents,
+    teams
   });
-
-//export * from "./coaches"
-//export * from "./managers"
-//export * from "./parents"
-//export * from "./teams"
