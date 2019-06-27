7437f3c9c415f65dda35a5addfa7a3858b6b2287
Nate <nate998877@gmail.com>
2019-06-27 14:24:24 -0400

    commented out the reducers again

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,15 @@
 import { connectRouter } from "connected-react-router";
 import { combineReducers } from "redux";
-import managers from "./managers";
-import coaches from "./coaches";
-import parents from "./parents";
-import teams from "./teams";
+// import managers from "./managers";
+// import coaches from "./coaches";
+// import parents from "./parents";
+// import teams from "./teams";
 
 export default history =>
   combineReducers({
     router: connectRouter(history),
-    coaches,
-    managers,
-    parents,
-    teams
+    // coaches,
+    // managers,
+    // parents,
+    // teams
   });
