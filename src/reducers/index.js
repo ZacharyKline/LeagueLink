5b359f04de80128909b801ff39ee8af004a975e2
Elizabeth Scheidt <escheidt5@gmail.com>
2019-06-27 14:03:49 -0400

    added store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,21 @@
-export * from "./coaches"
-export * from "./managers"
-export * from "./parents"
-export * from "./teams"
+import { combineReducers } from "redux";
+import { connectRouter } from "connected-react-router";
+
+//import coaches from "./coaches";
+//import managers from "./managers";
+//import parents from "./parents";
+//import teams from "./teams";
+
+export default history =>
+  combineReducers({
+    router: connectRouter(history)
+    //coaches,
+    //managers,
+    //parents,
+    //teams
+  });
+
+//export * from "./coaches"
+//export * from "./managers"
+//export * from "./parents"
+//export * from "./teams"
