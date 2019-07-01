ed0c0cc2cc2c9f7dddc4d142fd38dd99982fb07f
Elizabeth Scheidt <escheidt5@gmail.com>
2019-07-01 09:29:53 -0400

    progress on Registration steps starting to add teams.json and facility.json

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -14,21 +14,25 @@ class RegisterForm extends Component {
     lastName: "",
     email: "",
     phone: "",
+    teamName: "",
+    ageGroup: "",
     teamId: null,
     facilityId: null,
-    facilityName: null,
-    facilityAddress: null,
-    facilityCity: null,
-    facilityState: null,
+    facilityName: "",
+    facilityAddress: "",
+    facilityCity: "",
+    facilityState: "",
     facilityZipCode: null,
-    facilityPhone: null,
-    facilityEmail: null,
-    facilityUrl: null,
+    facilityPhone: "",
+    facilityEmail: "",
+    facilityUrl: "",
     passwordMatch: true,
     firstPage: true,
     secondPage: false,
     thirdPage: false,
-    radioValue: null
+    fourthPage: false,
+    confirmationPage: false,
+    radioValue: 1
   };
   /*handleRegister = e => {
     e.preventDefault();
@@ -44,13 +48,56 @@ class RegisterForm extends Component {
     }
   };
 */
+
+  handleSubmit = radioValue => e => {
+    if (radioValue === 1) {
+      console.log("Create Parent, Add Parent ID to Coach");
+    } else if (radioValue === 2) {
+      console.log("Create Coach, Create Team, Add Coach ID to Facility");
+    } else if (radioValue === 3) {
+      console.log("Create Manager, Create Facility");
+    }
+  };
+
   handleNext = num => e => {
-    e.preventDefault();
-    num === 1
-      ? this.setState({ firstPage: false, secondPage: true })
-      : num === 2 && this.state.radioValue !== null
-      ? this.setState({ secondPage: false, thirdPage: true })
-      : this.setState({ firstPage: false, secondPage: true });
+    //e.preventDefault();
+    if (
+      num === 1 &&
+      (this.state.firstName !== "" &&
+        this.state.lastName !== "" &&
+        this.state.email !== "" &&
+        this.state.phone !== "" &&
+        this.state.password !== "" &&
+        this.state.confirmPassword === this.state.password)
+    ) {
+      this.setState({ firstPage: false, secondPage: true });
+    }
+    if (num === 2) {
+      this.setState({ secondPage: false, thirdPage: true });
+    }
+    if (num === 3) {
+      if (
+        this.state.radioValue === 3 &&
+        this.state.facilityName !== "" &&
+        this.state.facilityAddress !== "" &&
+        this.state.facilityCity !== "" &&
+        this.state.facilityState !== "" &&
+        this.state.facilityZipCode !== "" &&
+        this.state.facilityEmail !== "" &&
+        this.state.facilityPhone !== ""
+      ) {
+        this.setState({ thirdPage: false, confirmationPage: true });
+      } else if (this.state.radioValue === 1) {
+        this.setState({ thirdPage: false, confirmationPage: true });
+      } else if (this.state.radioValue === 2) {
+        this.setState({ thirdPage: false, fourthPage: true });
+      }
+    }
+    if (num === 4) {
+      if (this.state.teamName !== "" && this.state.ageGroup !== "") {
+        this.setState({ fourthPage: false, confirmationPage: true });
+      }
+    }
   };
 
   handleBack = num => e => {
@@ -60,7 +107,12 @@ class RegisterForm extends Component {
       ? this.setState({ firstPage: true, secondPage: false })
       : num === 2
       ? this.setState({ secondPage: true, thirdPage: false })
-      : this.setState({ thirdPage: false });
+      : num === 3 &&
+        (this.state.radioValue === 3 || this.state.radioValue === 1)
+      ? this.setState({ thirdPage: true, confirmationPage: false })
+      : num === 3 && this.state.radioValue === 2
+      ? this.setState({ fourthPage: true, confirmationPage: false })
+      : this.setState();
   };
 
   handleChange = e => {
@@ -134,14 +186,15 @@ class RegisterForm extends Component {
                 addonBefore="Phone"
                 type="tel"
                 name="phone"
-                placeholder="000-000-0000"
-                //pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                placeholder="(000) 000-0000"
+                pattern="[(][0-9]{3}[)][\s][0-9]{3}-[0-9]{4}"
+                required
                 onChange={handleChange}
                 value={this.state.phone}
               />
             </Form.Item>
             <Form.Item>
-              <Input
+              <Input.Password
                 addonBefore="Password"
                 type="password"
                 name="password"
@@ -150,12 +203,13 @@ class RegisterForm extends Component {
                 onChange={handleChange}
               />
 
-              <Input
+              <Input.Password
                 addonBefore="Password"
                 type="password"
                 name="confirmPassword"
                 placeholder="Confirm"
                 value={this.state.confirmPassword}
+                pattern={this.state.password}
                 required
                 onChange={handleChange}
               />
@@ -169,7 +223,7 @@ class RegisterForm extends Component {
               }}
             >
               <button
-                type="submit"
+                //type="submit"
                 onClick={handleNext(1)}
                 style={{
                   background:
@@ -210,7 +264,7 @@ class RegisterForm extends Component {
                   fontSize: "large"
                 }}
               >
-                <Radio style={radioStyle} value={1}>
+                <Radio checked style={radioStyle} value={1}>
                   Athlete / Athlete's Legal Guardian?
                 </Radio>
                 <Radio style={radioStyle} value={2}>
@@ -360,6 +414,73 @@ class RegisterForm extends Component {
           </Form>
         )}
 
+        {this.state.fourthPage && (
+          <Form style={stylesForm}>
+            <RegisterHeader text={"Register Team"} />
+            <Form.Item>
+              <Input
+                addonBefore="Team Name"
+                type="text"
+                name="teamName"
+                required
+                onChange={handleChange}
+                value={this.state.teamName}
+              />
+
+              <Input
+                addonBefore="Age Group"
+                type="text"
+                name="ageGroup"
+                required
+                onChange={handleChange}
+                value={this.state.ageGroup}
+              />
+            </Form.Item>
+
+            <div
+              style={{
+                maxWidth: "100%",
+                display: "flex",
+                flexDirection: "row-reverse",
+                justifyContent: "space-between"
+              }}
+            >
+              <button
+                type="submit"
+                onClick={this.handleNext(4)}
+                style={{
+                  background:
+                    "linear-gradient(rgb(8, 77, 121),rgba(0, 53, 89, 1))",
+                  color: "rgb(161, 233, 29)",
+                  border: "2px rgb(130, 184, 31) solid",
+                  borderRadius: "25px",
+                  fontSize: "large",
+                  minWidth: "120px"
+                }}
+              >
+                Next
+                <Icon type="right" />
+              </button>
+              <button
+                type="submit"
+                onClick={this.handleBack(3)}
+                style={{
+                  background:
+                    "linear-gradient(rgb(8, 77, 121),rgba(0, 53, 89, 1))",
+                  color: "rgb(161, 233, 29)",
+                  border: "2px rgb(130, 184, 31) solid",
+                  borderRadius: "25px",
+                  fontSize: "large",
+                  minWidth: "120px"
+                }}
+              >
+                <Icon type="left" />
+                Back
+              </button>
+            </div>
+          </Form>
+        )}
+
         {this.state.thirdPage && this.state.radioValue === 3 && (
           <Form style={stylesForm}>
             <RegisterHeader text={"Register your Facility"} />
@@ -415,28 +536,30 @@ class RegisterForm extends Component {
             </Form.Item>
             <Form.Item>
               <Input
-                addonBefore="Phone"
-                type="tel"
-                name="facilityPhone"
-                placeholder="optional"
-                onChange={handleChange}
-                value={this.state.facilityPhone}
-              />
-              <Input
-                addonBefore="Email"
-                type="email"
-                name="facilityPhone"
-                placeholder="optional"
-                onChange={handleChange}
-                value={this.state.facilityEmail}
-              />
-              <Input
                 addonBefore="Website Url"
                 type="url"
                 name="facilityUrl"
                 placeholder="optional"
                 onChange={handleChange}
                 value={this.state.facilityUrl}
+              />
+              <Input
+                addonBefore="Email"
+                type="email"
+                name="facilityEmail"
+                required
+                onChange={handleChange}
+                value={this.state.facilityEmail}
+              />
+              <Input
+                addonBefore="Phone"
+                type="tel"
+                name="facilityPhone"
+                placeholder="(000) 000-0000"
+                pattern="[(][0-9]{3}[)][\s][0-9]{3}-[0-9]{4}"
+                required
+                onChange={handleChange}
+                value={this.state.facilityPhone}
               />
             </Form.Item>
 
@@ -467,6 +590,229 @@ class RegisterForm extends Component {
               <button
                 type="submit"
                 onClick={this.handleBack(2)}
+                style={{
+                  background:
+                    "linear-gradient(rgb(8, 77, 121),rgba(0, 53, 89, 1))",
+                  color: "rgb(161, 233, 29)",
+                  border: "2px rgb(130, 184, 31) solid",
+                  borderRadius: "25px",
+                  fontSize: "large",
+                  minWidth: "120px"
+                }}
+              >
+                <Icon type="left" />
+                Back
+              </button>
+            </div>
+          </Form>
+        )}
+
+        {this.state.confirmationPage && (
+          <Form style={stylesForm}>
+            <RegisterHeader text={"Confirm your Details"} />
+            <div
+              style={{
+                background: "rgba(0, 53, 89, 1)",
+                borderRadius: "5px"
+              }}
+            >
+              <Form.Item
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  padding: "15px",
+                  paddingLeft: "30px",
+                  fontSize: "large",
+                  color: "rgb(161, 233, 29)"
+                }}
+              >
+                <div
+                  style={{
+                    marginTop: "0px",
+                    marginBottom: "0px",
+                    maxHeight: "21px"
+                  }}
+                >
+                  <span>
+                    <b>{this.state.firstName}</b>
+                  </span>
+                  <span> </span>
+                  <span>
+                    <b>{this.state.lastName}</b>
+                  </span>
+                </div>
+                <div
+                  style={{
+                    marginTop: "0px",
+                    marginBottom: "0px",
+                    maxHeight: "21px"
+                  }}
+                >
+                  <span>{this.state.email}</span>
+                </div>
+                <div
+                  style={{
+                    marginTop: "0px",
+                    marginBottom: "0px",
+                    maxHeight: "21px"
+                  }}
+                >
+                  <span>{this.state.phone}</span>
+                </div>
+                {false &&
+                  (this.state.radioValue === 1 ||
+                    this.state.radioValue === 2) && (
+                    <div
+                      style={{
+                        marginTop: "0px",
+                        marginBottom: "0px",
+                        maxHeight: "21px"
+                      }}
+                    >
+                      <span>{this.state.teamIds}</span>
+                    </div>
+                  )}
+                {false && this.state.radioValue === 2 && (
+                  <div
+                    style={{
+                      marginTop: "0px",
+                      marginBottom: "0px",
+                      maxHeight: "21px"
+                    }}
+                  >
+                    <span>{this.state.facilityIds}</span>
+                  </div>
+                )}
+                {this.state.radioValue === 3 && (
+                  <React.Fragment>
+                    <br />
+                    <div
+                      style={{
+                        marginTop: "0px",
+                        marginBottom: "0px",
+                        maxHeight: "21px"
+                      }}
+                    >
+                      <span>
+                        <b>{this.state.facilityName}</b>
+                      </span>
+                    </div>
+                    <div
+                      style={{
+                        marginTop: "0px",
+                        marginBottom: "0px",
+                        maxHeight: "21px"
+                      }}
+                    >
+                      <span>{this.state.facilityAddress}</span>
+                    </div>
+                    <div
+                      style={{
+                        marginTop: "0px",
+                        marginBottom: "0px",
+                        maxHeight: "21px"
+                      }}
+                    >
+                      <span
+                        style={{
+                          marginTop: "0px",
+                          marginBottom: "0px",
+                          maxHeight: "21px"
+                        }}
+                      >
+                        {this.state.facilityCity}
+                      </span>
+                      <span>, </span>
+                      <span>{this.state.facilityState}</span>
+                      <span> </span>
+                      <span>{this.state.facilityZipCode}</span>
+                    </div>
+                    {this.state.facilityUrl !== "" && (
+                      <div
+                        style={{
+                          marginTop: "0px",
+                          marginBottom: "0px",
+                          maxHeight: "21px"
+                        }}
+                      >
+                        <span
+                          style={{
+                            marginTop: "0px",
+                            marginBottom: "0px",
+                            maxHeight: "21px"
+                          }}
+                        >
+                          {this.state.facilityUrl}
+                        </span>
+                      </div>
+                    )}
+                    <div
+                      style={{
+                        marginTop: "0px",
+                        marginBottom: "0px",
+                        maxHeight: "21px"
+                      }}
+                    >
+                      <span
+                        style={{
+                          marginTop: "0px",
+                          marginBottom: "0px",
+                          maxHeight: "21px"
+                        }}
+                      >
+                        {this.state.facilityEmail}
+                      </span>
+                    </div>
+                    <div
+                      style={{
+                        marginTop: "0px",
+                        marginBottom: "0px",
+                        maxHeight: "21px"
+                      }}
+                    >
+                      <span
+                        style={{
+                          marginTop: "0px",
+                          marginBottom: "0px",
+                          maxHeight: "21px"
+                        }}
+                      >
+                        {this.state.facilityPhone}
+                      </span>
+                    </div>
+                  </React.Fragment>
+                )}
+              </Form.Item>
+            </div>
+            <br />
+            <div
+              style={{
+                maxWidth: "100%",
+                display: "flex",
+                flexDirection: "row-reverse",
+                justifyContent: "space-between"
+              }}
+            >
+              <button
+                type="submit"
+                onClick={this.handleSubmit(this.state.radioValue)}
+                style={{
+                  background:
+                    "linear-gradient(rgb(8, 77, 121),rgba(0, 53, 89, 1))",
+                  color: "rgb(161, 233, 29)",
+                  border: "2px rgb(130, 184, 31) solid",
+                  borderRadius: "25px",
+                  fontSize: "large",
+                  minWidth: "120px"
+                }}
+              >
+                Next
+                <Icon type="right" />
+              </button>
+              <button
+                type="submit"
+                onClick={this.handleBack(3)}
                 style={{
                   background:
                     "linear-gradient(rgb(8, 77, 121),rgba(0, 53, 89, 1))",
