e8013d7051d69f84919b4d502aada9821f9cdc8b
Nathaniel Lyttle <nate998877@gmail.com>
2019-07-01 00:28:33 -0400

    Figured out how to remove the style from LoginForm and moved it to App.css

---
+++ src/components/TeamSchedule.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Calendar extends Component {
+  render() {
+    return (
+      <div>
+        <p>hello registration</p>
+      </div>
+    )
+  }
+}
--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -19,73 +19,62 @@ class LoginForm extends Component {
   };
 
 
-  
+
   render() {
     const { handleChange } = this
     const { isLoading, err } = this.props;
-    var stylesForm = {
-      color:'black',
-      backgroundColor:'lightgrey',
-      padding:'10px',
-      border:'1px black dashed',
-      width:'250px',
-      marginLeft:'50px',
-      marginTop:'100px'
-    };
     return (
       <React.Fragment>
         <Layout>
-        <Form style={stylesForm}>
-        <h1>Login</h1>
-          <label htmlFor="userName">Username</label>
-          <Form.Item>
-            
-            <Input
-              clasName={"stylee"}
-              //style={}
-              type="text"
-              name="username"
-              autoFocus
-              required
-              onChange={handleChange}
-            />
+          <Form className="myStyle">
+            <h1>Login</h1>
+            <label htmlFor="userName">Username</label>
+            <Form.Item>
+
+              <Input
+                className={"stylee"}
+                type="text"
+                name="username"
+                autoFocus
+                required
+                onChange={handleChange}
+              />
             </Form.Item >
             <label htmlFor="password">Password</label>
             <Form.Item>
-              
+
               <Input
-                clasName={"stylee"}
+                className={"stylee"}
                 type="password"
                 name="password"
                 required
                 onChange={handleChange}
               />
-          </Form.Item>
-          <Form.Item>
-            <Button 
-            className={"btn"}
-            type="submit" 
-            disabled={isLoading} 
-            onClick={this.handleLogin}>
-              Login
+            </Form.Item>
+            <Form.Item>
+              <Button
+                className={"btn"}
+                type="submit"
+                disabled={isLoading}
+                onClick={this.handleLogin}>
+                Login
             </Button>
-          </Form.Item>
-          {/*<Link to={'/registration'} className={"registerLink"}>Register</Link>*/}
-          {isLoading && <Spinner name="circle" color="blue" />}
-          {err && <p style={{ color: "red" }}>{err}</p>}
+            </Form.Item>
+            {/*<Link to={'/registration'} className={"registerLink"}>Register</Link>*/}
+            {isLoading && <Spinner name="circle" color="blue" />}
+            {err && <p style={{ color: "red" }}>{err}</p>}
 
-        </Form>
+          </Form>
         </Layout>
-        <Layout style={{height: "250px"}}>
-        </Layout>
+
       </React.Fragment>
-        );
-      }
-    }
-    
-    export default connect(
-  ({auth}) => ({
-          isLoading: auth.loginLoading,
-        err: auth.loginError
-      }),
-  {login})(LoginForm);
+    );
+  }
+}
+
+export default connect(
+  ({ auth }) => ({
+    isLoading: auth.loginLoading,
+    err: auth.loginError
+  }),
+  { login })(LoginForm);
