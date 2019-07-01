a610f06dcf4a448cc7473825a4fc6cc3e6bff29b
Zachary Kline <zkline888@gmail.com>
2019-07-01 08:50:29 -0400

    Committing the now finished Navbar and the basic layout of the calendar page

---
+++ src/components/UserProfile.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class UserProfile extends Component {
+    render(){
+        return(
+            <React.Fragment>
+                <h1>This is the user profile</h1>
+
+            </React.Fragment>
+        )
+    }
+}
+
+export default UserProfile
