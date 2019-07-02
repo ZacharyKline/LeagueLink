3d8eaf051aed7e7fdb9cb5f18e82d88567c5930c
Nathaniel Lyttle <nate998877@gmail.com>
2019-07-01 23:14:27 -0400

    Renamed Files with better descriptions

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ export { default as CalendarPage } from "./CalendarPage";
 
 export { default as RegisterHeader } from "./RegisterHeader";
 export { default as LoginForm } from "./LoginForm";
-export { default as UserProfile } from "./UserProfile";
+export { default as UserProfile } from "./UserProfilePage";
 // export { default as RegisterForm } from "./RegisterForm";
 
 //export { default as Menu } from "./Menu";
@@ -14,8 +14,7 @@ export { default as UserProfile } from "./UserProfile";
 //export { default as Logout } from "./Logout";
 
 export { default as InvalidUrlPage } from "./InvalidUrlPage";
-export { default as Landing } from "./Landing";
-export { default as Profile } from "./Profile";
-export { default as Login } from "./Login";
+export { default as LoginPage } from "./LoginPage";
+export { default as ProfilePage } from "./ProfilePage";
 export { default as RegistrationPage } from "./RegistrationPage";
-export { default as Register } from "./Register";
+export { default as RegistrationForm } from "./RegistrationForm";
