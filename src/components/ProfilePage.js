72f68c16de40b27abb8a0c28e85fde5714d064bb
Zachary Kline <zkline888@gmail.com>
2019-07-09 14:21:24 -0400

    Added Actions/reducers for edit profile

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -100,22 +100,6 @@ function onPanelChange(value, mode) {
 }
 
 
-/*
-import { Calendar } from 'antd';
-
-function onPanelChange(value, mode) {
-  console.log(value, mode);
-}
-
-ReactDOM.render(
-  <div style={{ width: 300, border: '1px solid #d9d9d9', borderRadius: 4 }}>
-    <Calendar fullscreen={false} onPanelChange={onPanelChange} />
-  </div>,
-  mountNode,
-);
-*/
-
-
 export default class Profile extends Component {
   render() {
     return (
