950886ba097c3e09745deebe3310e56845819b7c
Zachary Kline <zkline888@gmail.com>
2019-07-10 11:05:38 -0400

    Adding in the incomplete current Edit Profile funcitionality

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,7 @@ export { default as RegisterHeader } from "./RegisterHeader";
 export { default as LoginForm } from "./LoginForm";
 export { default as UserProfile } from "./UserProfilePage";
 // export { default as RegisterForm } from "./RegisterForm";
+export { default as EditProfile } from './EditProfilePage'
 
 //export { default as Menu } from "./Menu";
 //export { default as Settings } from "./Settings";
