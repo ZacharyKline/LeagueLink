f9440c286dc3a9dc40b1eda0e70208373874dc7d
Elizabeth Scheidt <escheidt5@gmail.com>
2019-07-11 14:00:43 -0400

    save before change branch

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,55 +1,89 @@
-//////////  Brett work 
+//////////  Brett work
 
-import React, { Component } from 'react'
-import { Navbar } from "."
-import { Row, Col, Card, Button, Calendar, Avatar, Popconfirm, message } from 'antd'
+import React, { Component } from "react";
+import { Navbar } from ".";
+import {
+  Row,
+  Col,
+  Card,
+  Button,
+  Calendar,
+  Avatar,
+  Popconfirm,
+  message
+} from "antd";
 // import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
-const text = 'Are you sure to delete this Account?';
+const text = "Are you sure to delete this Account?";
 
 function confirm() {
-  message.info('Clicked on Yes.');
+  message.info("Clicked on Yes.");
 }
 
 function onPanelChange(value, mode) {
   console.log(value, mode);
 }
 
-
 class Profile extends Component {
   render() {
     return (
-      <React.Fragment >
-        <section style={{ /* backgroundColor: "lightblue", alignItems:"center", margin:"auto", border: "1px rgba(0, 53, 89, 1) solid"  */ }}>
+      <React.Fragment>
+        <section
+          style={
+            {
+              /* backgroundColor: "lightblue", alignItems:"center", margin:"auto", border: "1px rgba(0, 53, 89, 1) solid"  */
+            }
+          }
+        >
           <Navbar />
 
-          <div style={{ backgroundColor: "lightblue", width: "1000px", margin: "auto", alignContent: "center", border: "5px darkblue solid", borderRadius: "25px" }} >
-            <div style={{ width: "800px", margin: "auto", alignContent: "center", border: "0", marginTop: "50px", }} >
-
-              <div style={{ marginBottom: "175px"  /*,marginTop:"50px" , */ }}>
-                <Col  >
-                  <Card style={{
-                    border: "2px rgba(0, 53, 89, 1) solid",
-                    borderRadius: "25px"
-                  }}>
+          <div
+            style={{
+              backgroundColor: "lightblue",
+              width: "1000px",
+              margin: "auto",
+              alignContent: "center",
+              border: "5px darkblue solid",
+              borderRadius: "25px"
+            }}
+          >
+            <div
+              style={{
+                width: "800px",
+                margin: "auto",
+                alignContent: "center",
+                border: "0",
+                marginTop: "50px"
+              }}
+            >
+              <div style={{ marginBottom: "175px" /*,marginTop:"50px" , */ }}>
+                <Col>
+                  <Card
+                    style={{
+                      border: "2px rgba(0, 53, 89, 1) solid",
+                      borderRadius: "25px"
+                    }}
+                  >
                     Teams
                     <br />
                     Team Name- Coaches Info
-                    </Card>
+                  </Card>
 
-                  <Card style={{
-                    border: "2px rgba(0, 53, 89, 1) solid",
-                    borderRadius: "25px"
-                  }} >
+                  <Card
+                    style={{
+                      border: "2px rgba(0, 53, 89, 1) solid",
+                      borderRadius: "25px"
+                    }}
+                  >
                     <Avatar shape="square" size={64} icon="user" />
                     <br />
                     Name: {this.props.fullName}
-                      <br />
+                    <br />
                     Contact Info {this.props.phone}
-                      <br />
-                    Team Affiliation 
-                    </Card>
+                    <br />
+                    Team Affiliation
+                  </Card>
                   {/* <Link to="/editprofile">
                     <Button type="dashed" style={{
                       border: "2px rgba(0, 53, 89, 1) solid",
@@ -58,26 +92,47 @@ class Profile extends Component {
                       Edit User
                       </Button>
                   </Link> */}
-                  <Popconfirm placement="top" title={text} onConfirm={confirm} okText="Yes" cancelText="No">
-                    <Button span={8} offset={4} type="dashed " style={{
-                      border: "2px rgba(0, 53, 89, 1) solid",
-                      borderRadius: "25px"
-                    }}>Delete Account</Button>
+                  <Popconfirm
+                    placement="top"
+                    title={text}
+                    onConfirm={confirm}
+                    okText="Yes"
+                    cancelText="No"
+                  >
+                    <Button
+                      span={8}
+                      offset={4}
+                      type="dashed "
+                      style={{
+                        border: "2px rgba(0, 53, 89, 1) solid",
+                        borderRadius: "25px"
+                      }}
+                    >
+                      Delete Account
+                    </Button>
                   </Popconfirm>
 
-                  <Card style={{
-                    border: "2px rgba(0, 53, 89, 1) solid",
-                    borderRadius: "25px", marginBottom: "0px"
-                  }}  >
-                    <div style={{ width: "100%", border: '1px solid #d9d9d9', borderRadius: 4 }}>
-                      <Calendar fullscreen={false} onPanelChange={onPanelChange} />
+                  <Card
+                    style={{
+                      border: "2px rgba(0, 53, 89, 1) solid",
+                      borderRadius: "25px",
+                      marginBottom: "0px"
+                    }}
+                  >
+                    <div
+                      style={{
+                        width: "100%",
+                        border: "1px solid #d9d9d9",
+                        borderRadius: 4
+                      }}
+                    >
+                      <Calendar
+                        fullscreen={false}
+                        onPanelChange={onPanelChange}
+                      />
                     </div>
-
                   </Card>
-
-
                 </Col>
-
               </div>
               {/* <Col span={8} offset={2}>
               </Col> */}
@@ -87,11 +142,11 @@ class Profile extends Component {
                   Something Righteous
               </Col> */}
               </Row>
-            </div >
+            </div>
           </div>
         </section>
       </React.Fragment>
-    )
+    );
   }
 }
 
@@ -102,16 +157,12 @@ function mapStateToProps({ users, auth }) {
     id: auth.login.id,
     fullName: users.user.fullName,
     phone: users.user.phone,
-    TeamIds: users.user.TeamIds,
-
+    TeamIds: users.user.TeamIds
   };
 }
-const mapDispatchToProps = {
-  
-}
+const mapDispatchToProps = {};
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(Profile);
-
