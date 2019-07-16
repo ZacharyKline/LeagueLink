965629edf03b8e8718e6adaa152d8509018e400c
Elizabeth Scheidt <escheidt5@gmail.com>
2019-07-16 07:43:33 -0400

    remove conditionals on navBar for now

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ import "../userConflicts.css";
 
 class Navbar extends Component {
   render() {
-    const { login } = this.props;
+    // const { login } = this.props;
 
     return (
       <React.Fragment>
@@ -21,14 +21,14 @@ class Navbar extends Component {
             </Link> */}
               </Menu.Item>
             )}
-            {login !== null && (
+            {
               <Menu.Item>
                 <Link to="/conflicts" style={{ color: "rgb(161, 233, 29)" }}>
                   My Conflicts
                 </Link>
               </Menu.Item>
-            )}
-            {login !== null && (
+            }
+            {
               <Menu.Item>
                 <Link
                   to="/teamconflicts"
@@ -37,36 +37,36 @@ class Navbar extends Component {
                   Team Conflicts
                 </Link>
               </Menu.Item>
-            )}
-            {login !== null && (
+            }
+            {
               <Menu.Item>
                 <Link to="/profile" style={{ color: "rgb(161, 233, 29)" }}>
                   Profile Page
                 </Link>
               </Menu.Item>
-            )}
-            {login === null && (
+            }
+            {
               <Menu.Item>
                 <Link to="/registration" style={{ color: "rgb(161, 233, 29)" }}>
                   Registration Page
                 </Link>
               </Menu.Item>
-            )}
-            {login === null && (
+            }
+            {
               <Menu.Item>
                 <Link to="/" style={{ color: "rgb(161, 233, 29)" }}>
                   Login Page
                 </Link>
               </Menu.Item>
-            )}
-            {login !== null && (
+            }
+            {
               <Menu.Item>
                 <div>
                   <Icon type="logout" />
                   <Logout />
                 </div>
               </Menu.Item>
-            )}
+            }
           </Menu>
         </div>
       </React.Fragment>
@@ -76,7 +76,7 @@ class Navbar extends Component {
 
 const mapStateToProps = state => {
   return {
-    login: state.auth.login
+    // login: state.auth.login
   };
 };
 
